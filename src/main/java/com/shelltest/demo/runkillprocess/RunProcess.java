2842831d614c89038080437fdaa2a5c0978c26fe
408770558 <a408770558@qq.com>
2019-08-22 09:47:25 +0800

    请求修改为 添加参数

--- src/main/java/com/shelltest/demo/runkillprocess/RunProcess.java
+++ src/main/java/com/shelltest/demo/runkillprocess/RunProcess.java
@@ -1,11 +1,14 @@
 package com.shelltest.demo.runkillprocess;
 
-import com.shelltest.demo.utils.RunUtils;
-import lombok.extern.log4j.Log4j2;
+import com.shelltest.demo.utils.Kernel32;
+import com.sun.jna.Platform;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 
 /**
  * 使用runtime调用第三方程序
@@ -13,51 +16,98 @@ import java.util.Arrays;
  * @author 18671
  */
 @RestController
-@Log4j2
 public class RunProcess {
 
     /**
-     * http://localhost:8080/sleepPid?command=vi%20/opt/1.txt
-     * @param command
-     * @return
+     * 执行脚本并返回pid值
+     *
+     * @param cmd 执行脚本
+     * @return -1执行失败 <0 为pid值
      */
-    @GetMapping("sleepPid")
-    public long sleepPid(String command) {
-        // 执行无阻塞调用
-        long pid = runJavaProcess(command);
+    @GetMapping("getPid")
+    public long getPid(String cmd) {
 
-        log.info("-----------" + pid);
-
-        //30秒后杀掉进程
+        //执行失败返回-1
+        long pid = -1;
         try {
-            Thread.sleep(1000 * 30);
-        } catch (InterruptedException e) {
+
+            Process child = Runtime.getRuntime().exec(cmd);
+
+            pid = getProcessPID(child);
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        stopProcess(pid);
-
         return pid;
     }
 
-    private static long runJavaProcess(String command) {
-        //String cmd = "java -cp D:\\example.JavaProcess1";
-        //String cmd = "\"D:\\notepad\\Notepad++\\notepad++.exe\" D:\\1.txt";
-        //String cmd = "vim /opt/sssss/xx.xx";
-
-        RunUtils.run(command);
-
-        // 获取pid
-        return (long) RunUtils.messageMap.get("pid");
+    /**
+     * 获取执行脚本的pid
+     *
+     * @param child jna返回参数
+     * @return -1执行失败 <0 为pid值
+     */
+    private static long getProcessPID(Process child) {
+        long pid = -1;
+        Field field;
+        try {
+            if (Platform.isWindows()) {
+                try {
+                    field = child.getClass().getDeclaredField("handle");
+                    field.setAccessible(true);
+                    pid = Kernel32.INSTANCE.GetProcessId((Long) field.get(child));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            } else if (Platform.isLinux()) {
+                try {
+                    Class<?> clazz = Class.forName("java.lang.UNIXProcess");
+                    field = clazz.getDeclaredField("pid");
+                    field.setAccessible(true);
+                    pid = (Integer) field.get(child);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return pid;
     }
 
-    private static void stopProcess(long pid) {
-        // 拼接命令
-        //String cmd = "taskkill /PID " + pid + " /F";
-        String cmd = "kiil -9" + pid;
-        // 运行命令
-        String[] returnContent = RunUtils.run2(cmd);
+    /**
+     * 根据cmd或pid查询进程是否存在
+     *
+     * @param commandOrPid cmd或pid值
+     * @return true 运行中， false 运行失败
+     */
+    @GetMapping("isExistPid")
+    public boolean isExistPid(String commandOrPid) {
 
-        System.out.println(Arrays.toString(returnContent));
+        BufferedReader reader = null;
+        try {
+            //显示所有进程
+            Process process = Runtime.getRuntime().exec("ps -ef");
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                if (line.contains(commandOrPid)) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
     }
 }
